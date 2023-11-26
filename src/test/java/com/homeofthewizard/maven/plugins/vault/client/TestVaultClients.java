b83db74eaae940de10195629148fc35d1dec5f3f
OZ <ozgunn.oz@gmail.com>
2023-11-26 14:43:09 +0100

    add tests for VaultClient

--- src/test/java/com/homeofthewizard/maven/plugins/vault/client/TestVaultClients.java
+++ src/test/java/com/homeofthewizard/maven/plugins/vault/client/TestVaultClients.java
@@ -13,8 +13,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.TreeMap;
+import java.util.NoSuchElementException;
 
 import static com.homeofthewizard.maven.plugins.vault.VaultTestHelper.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
